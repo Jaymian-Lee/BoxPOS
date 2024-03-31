2380983b570bf85906f394649c1920858282f76a
Jaymian-Lee <jaymian-lee@ziggo.nl>
2024-03-31 22:50:43 +0200

    feat(Overerving): overerving toegepast in service.cs

---
+++ Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Kassasysteem
+{
+    internal class Service : Product
+    {
+        // Extra eigenschap specifiek voor Service
+        public double ServiceDuration { get; set; }
+
+        // Constructor die de constructor van Product aanroept met het 'base' sleutelwoord
+        public Service(int id, string productName, string productDescription, double productPrice, int productQuantity, double serviceDuration)
+            : base(id, productName, productDescription, productPrice, productQuantity)
+        {
+            ServiceDuration = serviceDuration;
+        }
+
+    }
+}
